8265e24be3318c510e539815fcbd264a5dac0f4f
Alireza Zamani <alirezazamani2922@gmail.com>
2022-03-09 10:18:55 +0330

    feat: Format the date using date-fns
    Note that date-fns is only imported on the server-side

--- lib/id-to-date.ts
+++ lib/id-to-date.ts
@@ -1,7 +1,9 @@
+import { format } from "date-fns";
+
 const BASE_DATE = 1646000000000; // Mon Feb 28 2022 01:43:20 GMT+0330 (Iran Standard Time)
 
 /**
  * creates a dummy date based on the ID
  */
 export const idToDate = (id: number) =>
-  new Date(id * 100000000 + BASE_DATE).toString();
+  format(new Date(id * 100000000 + BASE_DATE), "MMMM dd, yyyy");
